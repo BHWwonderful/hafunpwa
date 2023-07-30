d68e95031e02f48412984b936684d524b48e0a27
BHWwonderful <wjdgksmf11267@gmail.com>
2023-07-30 17:26:06 +0900

    진행바 애니메이션 수정

--- src/pages/qna/index.js
+++ src/pages/qna/index.js
@@ -1,16 +1,12 @@
 // Components
 import HeaderContent from "../../components/semantics/HeaderContent";
 import Gnb from "../../components/semantics/Gnb";
-import Loading from "../../components/Loading";
 
 function Qna(){
 
     return(
       <div>
         <HeaderContent />
-        <section>
-          <Loading />
-        </section>
         <Gnb />
       </div>
     )
